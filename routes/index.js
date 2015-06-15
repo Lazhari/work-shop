b6f8a081a5dc4c0b49da3af85862bf1818e7a19c
Lazhari Mohammed <lazhari.mohammed@outlook.com>
2015-06-15 10:43:43 +0000

    Create service end point api

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,7 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' })
 };
+
+exports.apiEndpoint = function(req, res) {
+    return res.status(200).send({status:"success", results: "just a test !"});
+};
